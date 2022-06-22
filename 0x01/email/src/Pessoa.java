453281db935837431d5acde4db64c43cd0ce9376
CIANDT\pietros <pietros@ciandt.com>
2022-06-22 16:42:03 -0300

    email

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,12 @@
+
+
+public class Pessoa {
+    public static boolean emailValid(String email){
+        if (email.contains("@")){
+            if(email.length() <= 50){
+                return true;
+            }
+        }
+        return false;
+    }
+}
