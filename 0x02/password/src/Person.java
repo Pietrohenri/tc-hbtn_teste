ebb9a5e8cc2dd6a27ff714951bed43730066d2c6
CIANDT\pietros <pietros@ciandt.com>
2022-06-22 16:59:49 -0300

    password

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,18 @@
+
+
+public class Person {
+    public boolean checkUser(String username) {
+
+        String naoPermitidos = "[,.@$^?~=+\\\\-_\\\\]";
+
+        return !username.matches(naoPermitidos) || username.length() >= 8;
+    }
+
+
+
+    public boolean checkPassword(String password){
+
+        return !password.matches("/[^l-sB-Z\\h]*/o") || password.length() >= 8;
+
+    }
+}
